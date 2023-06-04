a86b8463dd2b88876550351809d1fbdde10cc041
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-04 22:11:01 +0200

    introduced secure keyring

--- mac_os_interface.py
+++ mac_os_interface.py
@@ -23,3 +23,25 @@ class NotificationManager:
         message (str): Die Nachricht, die in der Benachrichtigung angezeigt wird.
         """
         Notifier.notify(message, title=self.title)
+
+import keyring
+import sys
+
+class KeyNotFoundError(Exception):
+    pass
+
+class SecureKeyring:
+    def __init__(self, logger, key_name):
+        self.service_id = "MP4 Veröffentlichungs-Automation"
+        self.key_name = key_name
+        self.logger = logger
+
+    def get_key(self):
+        key = keyring.get_password(self.service_id, self.key_name)
+
+        if key is None:
+            self.logger.error(f"Key '{self.key_name}' not found. Please add this key manually to your MacOS Keyring.")
+            raise KeyNotFoundError(f"Key '{self.key_name}' not found.")
+        
+        return key
+
