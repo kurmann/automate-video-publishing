642c908fb602db79deeb4c86b7b6a81913d78466
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-07 16:40:50 +0200

    read first mp4 attributes with tag lib sharp

--- src/AutomateVideoPublishing/Program.cs
+++ src/AutomateVideoPublishing/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using TagLib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var filePath = "";
+        var file = TagLib.File.Create(filePath);
+
+        Console.WriteLine($"Title: {file.Tag.Title}");
+        Console.WriteLine($"Description: {file.Tag.Description}");
+    }
+}
