9a7903b2f66b2ac601262f56e5c699994b7ad76a
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-08 07:11:25 +0200

    Use MetadataExtractor to read quicktime header

--- src/AutomateVideoPublishing/Program.cs
+++ src/AutomateVideoPublishing/Program.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using TagLib;
+﻿using MetadataExtractor;
+using MetadataExtractor.Formats.QuickTime;
 
 class Program
 {
     static void Main(string[] args)
     {
         var filePath = "";
-        var file = TagLib.File.Create(filePath);
+        var directories = ImageMetadataReader.ReadMetadata(filePath);
 
-        Console.WriteLine($"Title: {file.Tag.Title}");
-        Console.WriteLine($"Description: {file.Tag.Description}");
+        var quickTimeMetadata = directories.OfType<QuickTimeMetadataHeaderDirectory>().FirstOrDefault();
+
+        if (quickTimeMetadata != null)
+        {
+            var descriptionTag = quickTimeMetadata.Tags.FirstOrDefault(t => t.Name == "Description");
+            var description = quickTimeMetadata.GetDescription(QuickTimeMetadataHeaderDirectory.TagDescription);
+            if (descriptionTag != null)
+            {
+                Console.WriteLine(descriptionTag.Description);
+            }
+            else
+            {
+                Console.WriteLine("Kein 'Description'-Tag gefunden.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Keine QuickTime Metadaten gefunden.");
+        }
     }
 }
