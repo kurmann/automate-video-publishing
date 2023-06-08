fda8d954bdce9e2466ed1a2eb32200318195c94b
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-08 16:47:23 +0200

    print all quicktime tags as json

--- src/AutomateVideoPublishing/Program.cs
+++ src/AutomateVideoPublishing/Program.cs
@@ -1,31 +1,57 @@
-﻿using MetadataExtractor;
+﻿using System.Text.Json;
+using MetadataExtractor;
 using MetadataExtractor.Formats.QuickTime;
+using CommandLine;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var filePath = "";
-        var directories = ImageMetadataReader.ReadMetadata(filePath);
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed<Options>(opts => RunCommand(opts));
+    }
 
-        var quickTimeMetadata = directories.OfType<QuickTimeMetadataHeaderDirectory>().FirstOrDefault();
-
-        if (quickTimeMetadata != null)
+    static void RunCommand(Options opts)
+    {
+        if (opts.File == null)
         {
-            var descriptionTag = quickTimeMetadata.Tags.FirstOrDefault(t => t.Name == "Description");
-            var description = quickTimeMetadata.GetDescription(QuickTimeMetadataHeaderDirectory.TagDescription);
-            if (descriptionTag != null)
+            Console.WriteLine("Datei-Parameter wurde nicht angegeben.");
+            return;
+        }
+
+        if (opts.ReadMetadata)
+        {
+            var directories = ImageMetadataReader.ReadMetadata(opts.File);
+            var quickTimeMetadata = directories.OfType<QuickTimeMetadataHeaderDirectory>().FirstOrDefault();
+
+            if (quickTimeMetadata != null)
             {
-                Console.WriteLine(descriptionTag.Description);
+                var quickTimeMetadataTags = quickTimeMetadata.Tags
+                    .Select(tag => new MetadataTag { Name = tag.Name, Description = tag.Description })
+                    .ToList();
+
+                var json = JsonSerializer.Serialize(quickTimeMetadataTags, new JsonSerializerOptions { WriteIndented = true });
+                Console.WriteLine(json);
             }
             else
             {
-                Console.WriteLine("Kein 'Description'-Tag gefunden.");
+                Console.WriteLine("Keine QuickTime Metadaten gefunden.");
             }
         }
-        else
-        {
-            Console.WriteLine("Keine QuickTime Metadaten gefunden.");
-        }
+    }
+
+    public class Options
+    {
+        [Option('r', "read-metadata", Required = false, HelpText = "Liest Metadaten aus der angegebenen Datei.")]
+        public bool ReadMetadata { get; set; }
+
+        [Value(0, MetaName = "file", HelpText = "Dateipfad.", Required = true)]
+        public string? File { get; set; }
+    }
+
+    public class MetadataTag
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }
