37ab29f0c5efcfae954f27f0dd59a01ef9f76b18
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-11 16:30:10 +0200

    working metadata reading for mp4 and quicktime

--- src/AutomateVideoPublishing/Entities/Mpeg4MetadataContainer.cs
+++ src/AutomateVideoPublishing/Entities/Mpeg4MetadataContainer.cs
@@ -1,7 +1,4 @@
-using TagLib;
 using CSharpFunctionalExtensions;
-using MetadataExtractor;
-using MetadataExtractor.Formats.QuickTime;
 
 namespace AutomateVideoPublishing.Entities;
 
@@ -10,45 +7,36 @@ public class Mpeg4MetadataContainer
     /// <summary>
     /// Enthält die Rohmetadaten, die aus der Datei gelesen wurden.
     /// </summary>
-    public TagLib.Tag[] Tags { get; }
+    public TagLib.Tag Tags { get; }
 
-    private Mpeg4MetadataContainer(TagLib.Tag[] tags) => Tags = tags;
+    private Mpeg4MetadataContainer(TagLib.Tag tags) => Tags = tags;
 
-    private static Result<IReadOnlyDictionary<string, string?>> TryGetMetadata(MediaFileInfoContainer fileContainer)
-    {
-        var directories = ImageMetadataReader.ReadMetadata(fileContainer.File.FullName);
-
-        var metadata = directories.OfType<QuickTimeMetadataHeaderDirectory>().FirstOrDefault();
-
-        if (metadata != null)
-        {
-            var metadataDict = metadata.Tags.ToDictionary(tag => tag.Name, tag => tag.Description);
-            return Result.Success((IReadOnlyDictionary<string, string?>)metadataDict);
-        }
-
-        return Result.Failure<IReadOnlyDictionary<string, string?>>("QuickTime metadata not found");
-    }
-
-    public static Result<Mpeg4MetadataContainer> Create(string? filePath)
+    private static Result<TagLib.Tag> TryGetMetadata(MediaFileInfoContainer fileContainer)
     {
         try
         {
-            var tagLibFile = TagLib.File.Create(filePath);
-            var tags = ((TagLib.CombinedTag)((TagLib.Mpeg4.File)tagLibFile).Tag).Tags;
-
-            var combinedTagByTagLib = tagLibFile.Tag;
-
-            var directories = ImageMetadataReader.ReadMetadata(filePath);
-            var metadata = directories.OfType<QuickTimeMetadataHeaderDirectory>().FirstOrDefault();
-            
-
-            return new Mpeg4MetadataContainer(tags);
+            var tagLibFile = TagLib.File.Create(fileContainer.File.FullName);
+            if (tagLibFile == null)
+            {
+                return Result.Failure<TagLib.Tag>($"File parsing of file ´{fileContainer.File.FullName}´ by TagLib returned null.");
+            }
+            var combinedTags = tagLibFile.Tag;
+            if (combinedTags == null)
+            {
+                return Result.Failure<TagLib.Tag>($"Combined tags of file ´{fileContainer.File.FullName}´ by TagLib is null.");
+            }
+            return Result.Success<TagLib.Tag>(combinedTags);
         }
         catch (Exception ex)
         {
-            return Result.Failure<Mpeg4MetadataContainer>(ex.Message);
+            return Result.Failure<TagLib.Tag>($"Error on reading metadata using Tag Lib: {ex.Message}");
         }
-
-        
     }
+
+    public static Result<Mpeg4MetadataContainer> Create(string? filePath) => MediaFileInfoContainer.Create(filePath)
+        .Bind(fileInfoContainer => TryGetMetadata(fileInfoContainer)
+        .Map(metadata => new Mpeg4MetadataContainer(metadata)));
+
+    public static Result<Mpeg4MetadataContainer> Create(MediaFileInfoContainer mediaFileInfoContainer) => TryGetMetadata(mediaFileInfoContainer)
+        .Map(metadata => new Mpeg4MetadataContainer(metadata));
 }
