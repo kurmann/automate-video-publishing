1d19d4771d33ca673adc9799d1dc95ae64d15594
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-16 22:33:01 +0200

    method chaining for cli params check and strategy

--- src/AutomateVideoPublishing/Program.cs
+++ src/AutomateVideoPublishing/Program.cs
@@ -1,6 +1,7 @@
 ﻿using AutomateVideoPublishing.Entities;
 using AutomateVideoPublishing.Strategies;
 using CommandLine;
+using CSharpFunctionalExtensions;
 
 class Program
 {
@@ -9,33 +10,24 @@ class Program
         var strategyMap = new Dictionary<string, IWorkflowStrategy>
         {
             { "TransmitMetadata", new TransmitMetadataStrategy() }
-            // Hier können Sie weitere Strategien hinzufügen.
+            // Weitere Strategien können hier hinzugefügt werden.
         };
 
         Parser.Default.ParseArguments<Options>(args)
             .WithParsed<Options>(opts =>
             {
-                var contextResult = WorkflowContext.Create(opts.SourceFile, opts.TargetFile);
-                if (contextResult.IsFailure)
-                {
-                    Console.WriteLine($"Error setting up workflow context: {contextResult.Error}");
-                    return;
-                }
-
-                if (string.IsNullOrWhiteSpace(opts.Strategy))
-                {
-                    Console.WriteLine("Strategy parameter cannot be empty.");
-                    return;
-                }
-                if (strategyMap.TryGetValue(opts.Strategy, out var strategy))
-                {
-                    strategy.Execute(contextResult.Value);
-                }
-                else
-                {
-                    Console.WriteLine("Unknown strategy: {0}", opts.Strategy);
-                }
+                opts.ValidateInputParameters()
+                    .Map(() => WorkflowContext.Create(opts.SourceFile, opts.TargetFile)
+                    .Map((context) => ExecuteStrategy(context, opts.Strategy ?? "TransmitMetadata", strategyMap))
+                    .Tap(result => Console.WriteLine(result.IsSuccess ? "Workflow completed" : result.Error)));
             });
+    }
+
+    private static Result ExecuteStrategy(WorkflowContext context, string strategyName, Dictionary<string, IWorkflowStrategy> strategyMap)
+    {
+        return strategyMap.ContainsKey(strategyName)
+            ? Result.Success(strategyMap[strategyName].Execute(context))
+            : Result.Failure($"Unknown strategy: {strategyName}");
     }
 }
 
@@ -47,6 +39,21 @@ public class Options
     [Option('t', "target", Required = true, HelpText = "Target file.")]
     public string? TargetFile { get; set; }
 
-    [Option('y', "strategy", Required = true, HelpText = "Strategy to execute.")]
+    [Option('y', "strategy", Required = false, HelpText = "Strategy to execute.")]
     public string? Strategy { get; set; }
+
+    public Result ValidateInputParameters()
+    {
+        if (string.IsNullOrWhiteSpace(SourceFile))
+        {
+            return Result.Failure("Source file cannot be empty.");
+        }
+
+        if (string.IsNullOrWhiteSpace(TargetFile))
+        {
+            return Result.Failure("Target file cannot be empty.");
+        }
+
+        return Result.Success();
+    }
 }
