214d29527f9dfe0e19aa28e7ff4f03b9d15ed975
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-18 09:50:13 +0200

    Created dedicated qt master direcory #19

---
+++ src/AutomateVideoPublishing/Entities/ValidQuickTimeMasterDirectory.cs
@@ -0,0 +1,40 @@
+/// <summary>
+/// Repräsentiert ein gültiges QuickTime-Master-Verzeichnis.
+/// Diese Klasse stellt sicher, dass das angegebene Verzeichnis existiert und enthält eine Sammlung von QuickTime-Dateien innerhalb dieses Verzeichnisses.
+/// </summary>
+public class ValidQuickTimeMasterDirectory
+{
+    /// <summary>
+    /// Das Verzeichnis
+    /// </summary>
+    public DirectoryInfo Directory { get; }
+
+    /// <summary>
+    /// Eine Sammlung von FileInfo-Objekten, die QuickTime-Dateien im Verzeichnis repräsentieren
+    /// </summary>
+    public IEnumerable<FileInfo> QuickTimeFiles { get; }
+
+    private ValidQuickTimeMasterDirectory(DirectoryInfo directory)
+    {
+        Directory = directory;
+        QuickTimeFiles = directory.EnumerateFiles("*.mov");
+    }
+
+    /// <summary>
+    /// Erstellt ein neues ValidQuickTimeMasterDirectory-Objekt.
+    /// </summary>
+    /// <param name="directoryPath">Der Pfad zum Verzeichnis</param>
+    /// <returns>Ein Result, das entweder das ValidQuickTimeMasterDirectory-Objekt im Erfolgsfall enthält, oder einen Fehler im Fehlerfall</returns>
+    public static Result<ValidQuickTimeMasterDirectory> Create(string? directoryPath)
+    {
+        directoryPath = string.IsNullOrWhiteSpace(directoryPath) ? "." : directoryPath;
+
+        var directory = new DirectoryInfo(directoryPath);
+        if (!directory.Exists)
+        {
+            return Result.Failure<ValidQuickTimeMasterDirectory>($"Das Verzeichnis {directoryPath} existiert nicht.");
+        }
+
+        return Result.Success(new ValidQuickTimeMasterDirectory(directory));
+    }
+}
