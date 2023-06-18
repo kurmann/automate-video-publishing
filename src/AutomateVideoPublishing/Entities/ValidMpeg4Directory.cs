ce20b26ec48e36a38b83d105fdf5954c675e7093
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-18 17:42:26 +0200

    ordinal ignore case for mpeg file extension #19

--- src/AutomateVideoPublishing/Entities/ValidMpeg4Directory.cs
+++ src/AutomateVideoPublishing/Entities/ValidMpeg4Directory.cs
@@ -1,18 +1,30 @@
 /// <summary>
-/// Repräsentiert ein gültiges MPEG-4-Verzeichnis.
-/// Diese Klasse stellt sicher, dass das angegebene Verzeichnis existiert und enthält eine Sammlung von MPEG-4-Dateien innerhalb dieses Verzeichnisses.
+/// Repräsentiert ein gültiges MPEG-4 Verzeichnis.
+/// Diese Klasse stellt sicher, dass das angegebene Verzeichnis existiert und enthält eine Sammlung von MPEG-4 Dateien (.mp4 oder .m4v) innerhalb dieses Verzeichnisses.
 /// </summary>
 public class ValidMpeg4Directory
 {
+    /// <summary>
+    /// Das Verzeichnis
+    /// </summary>
     public DirectoryInfo Directory { get; }
-    public IReadOnlyList<FileInfo> Mpeg4Files { get; }
+
+    /// <summary>
+    /// Eine Sammlung von FileInfo-Objekten, die MPEG-4 Dateien im Verzeichnis repräsentieren. Wenn keine Dateien gefunden werden, ist die Liste leer.
+    /// </summary>
+    public IEnumerable<FileInfo> Mpeg4Files { get; }
 
     private ValidMpeg4Directory(DirectoryInfo directory, List<FileInfo> mpeg4Files)
     {
         Directory = directory;
-        Mpeg4Files = mpeg4Files.AsReadOnly();
+        Mpeg4Files = mpeg4Files;
     }
 
+    /// <summary>
+    /// Erstellt ein neues ValidMpeg4Directory-Objekt.
+    /// </summary>
+    /// <param name="directoryPath">Der Pfad zum Verzeichnis</param>
+    /// <returns>Ein Result, das entweder das ValidMpeg4Directory-Objekt im Erfolgsfall enthält, oder einen Fehler im Fehlerfall</returns>
     public static Result<ValidMpeg4Directory> Create(string? directoryPath)
     {
         directoryPath = string.IsNullOrWhiteSpace(directoryPath) ? "." : directoryPath;
@@ -23,7 +35,9 @@ public class ValidMpeg4Directory
             return Result.Failure<ValidMpeg4Directory>("MPEG-4 directory does not exist.");
         }
 
-        var mpeg4Files = directory.EnumerateFiles("*.mp4").Concat(directory.EnumerateFiles("*.m4v")).ToList();
+        var mpeg4Files = directory.EnumerateFiles("*", SearchOption.TopDirectoryOnly)
+            .Where(f => string.Equals(f.Extension, ".mp4", StringComparison.OrdinalIgnoreCase)
+                    || string.Equals(f.Extension, ".m4v", StringComparison.OrdinalIgnoreCase)).ToList();
 
         return Result.Success(new ValidMpeg4Directory(directory, mpeg4Files));
     }
