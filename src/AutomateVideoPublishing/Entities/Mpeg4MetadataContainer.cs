5aac6ec3997e593e026c9461ed6d0d05f9559015
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-19 18:11:26 +0200

    renamed class #19

--- src/AutomateVideoPublishing/Entities/Mpeg4MetadataContainer.cs
+++ src/AutomateVideoPublishing/Entities/Mpeg4MetadataContainer.cs
@@ -1,13 +1,13 @@
 namespace AutomateVideoPublishing.Entities;
 
-public class Mpeg4MediaFileContainer : MediaFileInfoContainer
+public class Mpeg4MetadataContainer : MediaFileInfoContainer
 {
     /// <summary>
     /// Enthält die Rohmetadaten, die aus der Datei gelesen wurden.
     /// </summary>
     public TagLib.Tag Tags { get; private set; }
 
-    private Mpeg4MediaFileContainer(FileInfo file, MediaType mediaType, TagLib.Tag tags) : base(file, mediaType)
+    private Mpeg4MetadataContainer(FileInfo file, MediaType mediaType, TagLib.Tag tags) : base(file, mediaType)
     {
         Tags = tags;
     }
@@ -34,26 +34,26 @@ public class Mpeg4MediaFileContainer : MediaFileInfoContainer
         }
     }
 
-    public static new Result<Mpeg4MediaFileContainer> Create(string? filePath)
+    public static new Result<Mpeg4MetadataContainer> Create(string? filePath)
     {
         var result = MediaFileInfoContainer.Create(filePath);
         if (result.IsFailure)
         {
-            return Result.Failure<Mpeg4MediaFileContainer>(result.Error);
+            return Result.Failure<Mpeg4MetadataContainer>(result.Error);
         }
 
         if (result.Value.MediaType != MediaType.Mpeg4)
         {
-            return Result.Failure<Mpeg4MediaFileContainer>($"File {filePath} is not a MPEG-4 .m4v or .mp4 file.");
+            return Result.Failure<Mpeg4MetadataContainer>($"File {filePath} is not a MPEG-4 .m4v or .mp4 file.");
         }
 
         var metadataResult = TryGetMetadata(result.Value.File);
         if (metadataResult.IsFailure)
         {
-            return Result.Failure<Mpeg4MediaFileContainer>(metadataResult.Error);
+            return Result.Failure<Mpeg4MetadataContainer>(metadataResult.Error);
         }
 
-        return Result.Success(new Mpeg4MediaFileContainer(result.Value.File, result.Value.MediaType, metadataResult.Value));
+        return Result.Success(new Mpeg4MetadataContainer(result.Value.File, result.Value.MediaType, metadataResult.Value));
     }
 }
 
