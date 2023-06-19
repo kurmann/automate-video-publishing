9f558a84b4ebcfe7107f0a0d1852bf3329d2c41d
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-19 09:38:08 +0200

    workflow context doc

--- src/AutomateVideoPublishing/Entities/WorkflowContext.cs
+++ src/AutomateVideoPublishing/Entities/WorkflowContext.cs
@@ -1,8 +1,21 @@
 public class WorkflowContext
 {
-    private const string DefaultDirectory = ".";  // Aktuelles Verzeichnis
+    /// <summary>
+    /// Das Standardverzeichnis falls QuickTimeMasterDirectory und/oder PublishedMpeg4Directory keine Werte übergeben werden
+    /// bei der Instanziierung.
+    /// </summary>
+    public const string DefaultDirectory = ".";  // Aktuelles Verzeichnis
 
+    /// <summary>
+    /// Das Verzeichnis indem sich die QuickTime MOV-Masterdateien befinden.
+    /// </summary>
+    /// <value></value>
     public ValidQuickTimeMasterDirectory QuickTimeMasterDirectory { get; }
+
+    /// <summary>
+    /// Das Verzeichnis der MPEG-4 Dateien, die aus der Masterdatei komprimiert wurden.
+    /// </summary>
+    /// <value></value>
     public ValidMpeg4Directory PublishedMpeg4Directory { get; }
 
     private WorkflowContext(ValidQuickTimeMasterDirectory quickTimeMasterDirectory, ValidMpeg4Directory publishedMpeg4Directory)
@@ -11,6 +24,12 @@ public class WorkflowContext
         PublishedMpeg4Directory = publishedMpeg4Directory;
     }
 
+    /// <summary>
+    /// Erstellt einen neuen invarianten Workflow-Context
+    /// </summary>
+    /// <param name="quickTimeMasterDirectoryPath">Das Verzeichnis indem sich die QuickTime MOV-Masterdateien befinden.</param>
+    /// <param name="publishedMpeg4DirectoryPath">Das Standardverzeichnis falls QuickTimeMasterDirectory und/oder PublishedMpeg4Directory.</param>
+    /// <returns></returns>
     public static Result<WorkflowContext, string> Create(string? quickTimeMasterDirectoryPath, string? publishedMpeg4DirectoryPath)
     {
         // Use default values if none are provided
