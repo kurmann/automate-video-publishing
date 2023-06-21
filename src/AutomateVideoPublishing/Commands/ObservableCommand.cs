6819b0f60f8cd7e6e0dd1bdf0b77db975c483c8e
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-21 12:40:31 +0200

    added detailed code doc #11

--- src/AutomateVideoPublishing/Commands/ObservableCommand.cs
+++ src/AutomateVideoPublishing/Commands/ObservableCommand.cs
@@ -1,3 +1,7 @@
+// Die IObservableCommand<T>-Schnittstelle erweitert die IObservable<T>-Schnittstelle 
+// um die Fähigkeit, einen Workflow-Kontext auszuführen. Sie definiert zwei Methoden:
+// 1. Execute: führt den Workflow-Kontext aus und gibt das Ergebnis als Result<T>-Objekt zurück.
+// 2. Subscribe: ermöglicht es Observers, sich bei diesem Observable zu registrieren.
 public interface IObservableCommand<T> : IObservable<T>
 {
     Task<Result<T>> Execute(WorkflowContext context);
--- src/AutomateVideoPublishing/Commands/Unsubscriber.cs
+++ src/AutomateVideoPublishing/Commands/Unsubscriber.cs
@@ -1,14 +1,24 @@
+// Die Unsubscriber<T>-Klasse dient als Hilfsklasse, um die Beziehung zwischen einem 
+// Observable und einem Observer zu beenden. Sie implementiert das IDisposable-Interface, 
+// damit sie in einem using-Block oder durch Aufruf der Dispose-Methode verwendet werden kann.
 public class Unsubscriber<T> : IDisposable
 {
-    private readonly List<IObserver<T>> _observers;
-    private readonly IObserver<T> _observer;
+    // Wir speichern eine Referenz auf die Liste der Observer, die beim Observable registriert sind.
+    private List<IObserver<T>> _observers;
 
+    // Wir speichern auch eine Referenz auf den Observer, den wir entfernen möchten.
+    private IObserver<T> _observer;
+
+    // Im Konstruktor speichern wir die Referenzen auf die Observer-Liste und den spezifischen Observer.
     public Unsubscriber(List<IObserver<T>> observers, IObserver<T> observer)
     {
         _observers = observers;
         _observer = observer;
     }
 
+    // Wenn Dispose aufgerufen wird (entweder manuell oder automatisch durch einen using-Block), 
+    // entfernen wir den Observer aus der Liste. Dadurch wird die Beziehung zwischen dem Observable 
+    // und diesem Observer beendet.
     public void Dispose()
     {
         if (_observers.Contains(_observer))
