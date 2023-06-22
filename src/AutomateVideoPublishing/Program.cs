cb43916162498aa20e756e9bda81db3411b45c53
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-22 07:48:14 +0200

    main method docs #19

--- src/AutomateVideoPublishing/Program.cs
+++ src/AutomateVideoPublishing/Program.cs
@@ -24,11 +24,11 @@ class Program
             return;
         }
         
-        // Erstellen Sie den Observer und abonnieren Sie ihn bei der Strategy
+        // Abonniere die Statusnachrichten von der gewählten Strategie
         var logObserver = new LogObserver();
         strategyResult.Value.SelectedStrategy.EventBroadcaster.Subscribe(logObserver);
 
-                // Workflow-Kontext erstellen
+        // Workflow-Kontext erstellen
         var contextResult = WorkflowContext.Create(options.QuickTimeMasterDirectory, options.PublishedMpeg4Directory)
             .Tap(context => Logger.Info($"Executing workflow with quick time masterfile directory: {context.QuickTimeMasterDirectory.Directory}"))
             .Tap(context => Logger.Info($"Executing workflow with published MPEG-4 directory: {context.PublishedMpeg4Directory.Directory}"));
@@ -41,7 +41,7 @@ class Program
         // Ausführung der Strategie
         var executionResult = strategyResult.Value.SelectedStrategy.Execute(contextResult.Value);
 
-        // Falls Sie den Observer hier beenden möchten
+        // Log-Observer abschliessen
         logObserver.OnCompleted();
     }
 }
