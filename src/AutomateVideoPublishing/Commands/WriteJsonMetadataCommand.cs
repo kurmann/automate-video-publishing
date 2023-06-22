53d6e63da9bc53a77f45685ac07c5da20a2fb987
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-22 14:43:20 +0200

    document WriteJsonMetadataCommand

--- src/AutomateVideoPublishing/Commands/WriteJsonMetadataCommand.cs
+++ src/AutomateVideoPublishing/Commands/WriteJsonMetadataCommand.cs
@@ -8,8 +8,15 @@ public class WriteJsonMetadataCommand : ICommand<string>
 {
     private readonly QuickTimeMetadataReadCommand _quickTimeCommand;
     private readonly Mpeg4DirectoryMetadataReadCommand _mpeg4Command;
+
+    /// <summary>
+    /// Subject, das den Dateipfad von erfolgreich erstellten JSON-Dateien sendet.
+    /// </summary>
     private readonly Subject<string> _broadcaster = new Subject<string>();
 
+    /// <summary>
+    /// Beobachtbare Strömung von Dateipfaden aus erfolgreich erstellten JSON-Dateien.
+    /// </summary>
     public IObservable<string> WhenDataAvailable => _broadcaster.AsObservable();
 
     public WriteJsonMetadataCommand(QuickTimeMetadataReadCommand quickTimeCommand, Mpeg4DirectoryMetadataReadCommand mpeg4Command)
@@ -20,7 +27,7 @@ public class WriteJsonMetadataCommand : ICommand<string>
 
     public void Execute(WorkflowContext context)
     {
-        // Merge the data streams from both commands into one
+        // Zusammenführen der Datenströme beider Befehle zu einem
         _quickTimeCommand.WhenDataAvailable
             .SelectMany(container => WriteJsonAsync(container.FileInfo).ToObservable())
             .Merge(_mpeg4Command.WhenDataAvailable.SelectMany(container => WriteJsonAsync(container.FileInfo).ToObservable()))
@@ -32,6 +39,11 @@ public class WriteJsonMetadataCommand : ICommand<string>
         _broadcaster.OnCompleted();
     }
 
+    /// <summary>
+    /// Schreibt JSON-Daten in eine Datei und gibt den Dateipfad zurück. Tritt ein Fehler auf, wird ein Fehler gesendet und ein leerer String zurückgegeben.
+    /// </summary>
+    /// <param name="fileInfo">Die Dateiinformationen, aus denen die JSON-Daten generiert werden.</param>
+    /// <returns>Den Dateipfad der erfolgreich erstellten JSON-Datei oder einen leeren String im Fehlerfall.</returns>
     private async Task<string> WriteJsonAsync(FileInfo fileInfo)
     {
         var jsonResult = MediaMetadataJson.Create(fileInfo.FullName);
@@ -43,7 +55,7 @@ public class WriteJsonMetadataCommand : ICommand<string>
         }
         else
         {
-            _broadcaster.OnError(new Exception($"Json file from file {fileInfo} could not be generated."));
+            _broadcaster.OnError(new Exception($"Json-Datei aus Datei {fileInfo} konnte nicht generiert werden."));
             return string.Empty;
         }
     }
