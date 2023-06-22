3308b19e8580d952ab501c164a06cb6926d1e17d
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-22 07:32:30 +0200

    removed obsolete strategy observer #11

--- src/AutomateVideoPublishing/Strategies/ReadAllMetadataStrategyObserver.cs
+++
@@ -1,6 +0,0 @@
-public class ReadAllMetadataStrategyObserver : WorkflowStrategyObserver<FileInfo>
-{
-    public ReadAllMetadataStrategyObserver() : base(nameof(ReadAllMetadataStrategy)) { }
-
-    public override void OnNext(FileInfo value) => Broadcast($"New Json file created: {value.FullName}");
-}
