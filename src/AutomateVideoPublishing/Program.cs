290e8f263ca5cff9b3fd2866507bd60cd77601a5
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-22 07:35:17 +0200

    integrated strategy with broadcaster #19

--- src/AutomateVideoPublishing/Program.cs
+++ src/AutomateVideoPublishing/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using AutomateVideoPublishing.Strategies;
+using Microsoft.Extensions.Configuration;
 using NLog;
 
 class Program
@@ -15,21 +16,33 @@ class Program
         var options = new Options();
         configuration.Bind(options);
 
-        // Workflow-Kontext erstellen
+        // Gewählte Strategie instanzieren
+        var strategyResult = WorkflowStrategyMapper.Create(options.Workflow);
+        if (strategyResult.IsFailure)
+        {
+            Logger.Error($"Error on selecting strategy: {strategyResult.Error}");
+            return;
+        }
+        
+        // Erstellen Sie den Observer und abonnieren Sie ihn bei der Strategy
+        var logObserver = new LogObserver();
+        strategyResult.Value.SelectedStrategy.EventBroadcaster.Subscribe(logObserver);
+
+                // Workflow-Kontext erstellen
         var contextResult = WorkflowContext.Create(options.QuickTimeMasterDirectory, options.PublishedMpeg4Directory)
             .Tap(context => Logger.Info($"Executing workflow with quick time masterfile directory: {context.QuickTimeMasterDirectory.Directory}"))
             .Tap(context => Logger.Info($"Executing workflow with published MPEG-4 directory: {context.PublishedMpeg4Directory.Directory}"));
         if (contextResult.IsFailure)
         {
-            Logger.Info($"Error setting up workflow context: {contextResult.Error}");
+            Logger.Error($"Error createing worfklow context: {contextResult.Error}");
             return;
         }
 
-        // Strategie mappen und ausführen
-        var workflowResult = WorkflowStrategyMapper.Create(options.Workflow)
-            .Map(strategyMapper => strategyMapper.SelectedStrategyHandler.Execute(contextResult.Value));
-        Logger.Info(workflowResult.IsFailure ? workflowResult.Error : "Workflow completed");
+        // Ausführung der Strategie
+        var executionResult = strategyResult.Value.SelectedStrategy.Execute(contextResult.Value);
 
+        // Falls Sie den Observer hier beenden möchten
+        logObserver.OnCompleted();
     }
 }
 
