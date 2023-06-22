a9ddffb2e53bd7fbc0c2f922515d1ad5ce7610a6
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-22 09:30:04 +0200

    use of dispose and explain in detail #19

--- src/AutomateVideoPublishing/Program.cs
+++ src/AutomateVideoPublishing/Program.cs
@@ -26,7 +26,7 @@ class Program
         
         // Abonniere die Statusnachrichten von der gewählten Strategie
         var logObserver = new LogObserver();
-        strategyResult.Value.SelectedStrategy.EventBroadcaster.Subscribe(logObserver);
+        var strategyUnsubscriber = strategyResult.Value.SelectedStrategy.EventBroadcaster.Subscribe(logObserver);
 
         // Workflow-Kontext erstellen
         var contextResult = WorkflowContext.Create(options.QuickTimeMasterDirectory, options.PublishedMpeg4Directory)
@@ -41,8 +41,20 @@ class Program
         // Ausführung der Strategie
         var executionResult = strategyResult.Value.SelectedStrategy.Execute(contextResult.Value);
 
-        // Log-Observer abschliessen
+        // Diese Methode wird aufgerufen, um dem LogObserver zu signalisieren, dass das Observable, auf das er
+        // abonniert hat (in diesem Fall der EventBroadcaster der gewählten Strategie), keine weiteren Daten 
+        // senden wird. Nachdem diese Methode aufgerufen wurde, sollte der LogObserver keine weiteren 
+        // OnNext- oder OnError-Aufrufe von diesem Observable erwarten. 
         logObserver.OnCompleted();
+
+        // Diese Methode wird aufgerufen, um den LogObserver effektiv von der Liste der Observer des 
+        // Observables zu entfernen. Durch Aufrufen dieser Methode teilen wir dem Observable mit, dass es keine
+        // weiteren Daten an den LogObserver senden soll. Diese Methode ist besonders nützlich, wenn das Observable
+        // weiter Daten senden könnte, der spezifische Observer (in diesem Fall der LogObserver) jedoch keine
+        // weiteren Daten empfangen möchte. In diesem speziellen Fall, in dem das Programm kurz danach endet, 
+        // ist es möglicherweise nicht notwendig, diese Methode aufzurufen, jedoch sorgt dies für eine saubere
+        // und korrekte Verwendung des Observer-Patterns.
+        strategyUnsubscriber.Dispose();
     }
 }
 
