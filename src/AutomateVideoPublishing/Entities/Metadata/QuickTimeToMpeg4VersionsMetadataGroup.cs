d206c64e7c0090e319d2bf13a00a580a923070ea
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-24 22:43:10 +0200

    logic for grouping mpeg4 from same mov #13

---
+++ src/AutomateVideoPublishing/Entities/Metadata/QuickTimeToMpeg4VersionsMetadataGroup.cs
@@ -0,0 +1,56 @@
+namespace AutomateVideoPublishing.Entities.Metadata;
+
+public class QuickTimeToMpeg4VersionsMetadataGroup
+{
+    public QuickTimeMetadataContainer QuickTimeMetadataContainer { get; }
+    public List<Mpeg4MetadataContainer> Mpeg4MetadataContainers { get; }
+
+    private QuickTimeToMpeg4VersionsMetadataGroup(QuickTimeMetadataContainer quickTimeMetadataContainer, List<Mpeg4MetadataContainer> mpeg4MetadataContainers)
+    {
+        QuickTimeMetadataContainer = quickTimeMetadataContainer;
+        Mpeg4MetadataContainers = mpeg4MetadataContainers;
+    }
+
+    public static Result<QuickTimeToMpeg4VersionsMetadataGroup> Create(string quickTimeFilePath, DirectoryInfo targetDirectory)
+    {
+        var quickTimeContainerResult = QuickTimeMetadataContainer.Create(quickTimeFilePath);
+
+        if (quickTimeContainerResult.IsFailure)
+        {
+            return Result.Failure<QuickTimeToMpeg4VersionsMetadataGroup>(quickTimeContainerResult.Error);
+        }
+
+        var quickTimeFileInfo = new FileInfo(quickTimeFilePath);
+
+        // Extract the date and name from the source file.
+        var dateAndName = Path.GetFileNameWithoutExtension(quickTimeFileInfo.Name);
+        var mpeg4MetadataContainers = new List<Mpeg4MetadataContainer>();
+
+        // Look for all mpeg4 files in the target directory that start with the same date and name.
+        var mpeg4Files = targetDirectory.GetFiles($"{dateAndName}*.m4v")
+                            .Concat(targetDirectory.GetFiles($"{dateAndName}*.mp4"))
+                            .Concat(targetDirectory.GetFiles($"{dateAndName}*.mv4"));
+
+        foreach (var mpeg4File in mpeg4Files)
+        {
+            var mpeg4ContainerResult = Mpeg4MetadataContainer.Create(mpeg4File.FullName);
+
+            if (mpeg4ContainerResult.IsFailure)
+            {
+                return Result.Failure<QuickTimeToMpeg4VersionsMetadataGroup>(mpeg4ContainerResult.Error);
+            }
+
+            mpeg4MetadataContainers.Add(mpeg4ContainerResult.Value);
+        }
+
+        if (!mpeg4MetadataContainers.Any())
+        {
+            return Result.Failure<QuickTimeToMpeg4VersionsMetadataGroup>("No matching mpeg4 files found in target directory.");
+        }
+
+        return Result.Success(new QuickTimeToMpeg4VersionsMetadataGroup(quickTimeContainerResult.Value, mpeg4MetadataContainers));
+    }
+
+
+    // Methoden zum Überprüfen und Anwenden von Metadatenänderungen können hier eingefügt werden.
+}
