0d8da65aa5b74f085f55cddbba208d1485a92cf1
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-24 07:42:49 +0200

    added another entity namespace #14

--- src/AutomateVideoPublishing/Usings.cs
+++ src/AutomateVideoPublishing/Usings.cs
@@ -1,3 +1,4 @@
 global using CSharpFunctionalExtensions;
 global using AutomateVideoPublishing.Entities;
+global using AutomateVideoPublishing.Entities.Elementary;
 global using  AutomateVideoPublishing.Entities.Metadata;
