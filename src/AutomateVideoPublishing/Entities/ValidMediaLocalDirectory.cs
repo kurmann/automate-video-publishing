b6e778c73e32ec247017e9e3c0102bd0404cd284
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-06-24 06:37:01 +0200

    defined valid media local directory #14

---
+++ src/AutomateVideoPublishing/Entities/ValidMediaLocalDirectory.cs
@@ -0,0 +1,35 @@
+namespace AutomateVideoPublishing.Entities;
+
+public class ValidMediaLocalDirectory
+{
+    /// <summary>
+    /// Der vollständige Pfad zum Verzeichnis. Beispiel: "C:\MyFolder\MySubFolder"
+    /// </summary>
+    public string FullPath { get; }
+
+    // Privater Konstruktor, der nur innerhalb dieser Klasse aufgerufen werden kann.
+    private ValidMediaLocalDirectory(string path) => FullPath = path;
+
+    /// <summary>
+    /// Erstellt eine ValidMediaLocalDirectory-Instanz aus einem Verzeichnispfad.
+    /// Überprüft, ob der bereitgestellte Pfad null oder leer ist und ob das Verzeichnis existiert.
+    /// </summary>
+    /// <param name="path">Der Pfad, aus dem die ValidMediaLocalDirectory erstellt werden soll.</param>
+    /// <returns>Eine Result-Instanz, die entweder eine ValidMediaLocalDirectory-Instanz oder eine Fehlermeldung enthält.</returns>
+    public static Result<ValidMediaLocalDirectory> Create(string path)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            return Result.Failure<ValidMediaLocalDirectory>("Directory path cannot be null or empty.");
+        }
+
+        var directoryInfo = new DirectoryInfo(path);
+
+        if (!directoryInfo.Exists)
+        {
+            return Result.Failure<ValidMediaLocalDirectory>($"Directory does not exist: {directoryInfo.FullName}");
+        }
+
+        return Result.Success(new ValidMediaLocalDirectory(directoryInfo.FullName));
+    }
+}
