53448019e936815e439981607025e551b0d39eb8
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-07-01 14:41:01 +0200

    AppleVideoMetadata #13

--- src/AutomateVideoPublishing/Entities/Metadata/AppleVideoMetadata.cs
+++ src/AutomateVideoPublishing/Entities/Metadata/AppleVideoMetadata.cs
@@ -1,10 +1,70 @@
+using System.Text.RegularExpressions;
+
 namespace AutomateVideoPublishing.Entities.Metadata;
 
 /// <summary>
 /// Klasse zur Darstellung der Metadaten für ein Video.
 /// </summary>
-public class VideoMetadata
+public class AppleVideoMetadata
 {
+    public static Result<AppleVideoMetadata> Create(AtomicParsleyMetadataReadResult atomicParsleyMetadata)
+    {
+        // Überprüfen Sie, ob atomicParsleyMetadata gültige Daten hat
+        if (atomicParsleyMetadata == null || !atomicParsleyMetadata.Lines.Any())
+        {
+            return Result.Failure<AppleVideoMetadata>("AtomicParsleyMetadataReadResult ist ungültig oder hat keine Zeilen.");
+        }
+
+        var metadata = new AppleVideoMetadata();
+
+        // Versuchen Sie, jede Zeile zu analysieren und in den entsprechenden Metadaten zu speichern
+        foreach (var line in atomicParsleyMetadata.Lines)
+        {
+            if (TryParseAtomicParsleyResult(line, out var atom, out var content))
+            {
+                switch (atom)
+                {
+                    case "©nam":
+                        metadata.Title = content != null ? content : Maybe.None;
+                        break;
+                    case "©day":
+                        if (DateTime.TryParse(content, out var date))
+                        {
+                            metadata.Day = date;
+                        }
+                        break;
+                    case "©art":
+                        metadata.Artist = content != null ? content : Maybe.None;
+                        break;
+                    case "©des":
+                        metadata.Description = content != null ? content : Maybe.None;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        return Result.Success(metadata);
+    }
+
+    private static bool TryParseAtomicParsleyResult(string line, out string? atom, out string? content)
+    {
+        var match = Regex.Match(line, @"Atom ""(.*?)"" contains: (.*)");
+
+        if (match.Success)
+        {
+            atom = match.Groups[1].Value;
+            content = match.Groups[2].Value;
+            return true;
+        }
+
+        atom = null;
+        content = null;
+        return false;
+    }
+
+
     /// <summary>
     /// Setzt den Namen der TV Show auf dem "tvsh" Atom.
     /// </summary>
@@ -43,7 +103,7 @@ public class VideoMetadata
     /// <summary>
     /// Setzt den Jahr-Tag auf "moov.udta.meta.ilst.©day.data".
     /// </summary>
-    public Maybe<DateTime> Year { get; set; }
+    public Maybe<DateTime> Day { get; set; }
 
     /// <summary>
     /// Setzt den Künstler-Tag auf "moov.udta.meta.ilst.©ART.data".
