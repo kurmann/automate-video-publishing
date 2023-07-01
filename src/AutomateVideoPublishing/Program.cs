e7d6220e0bf08294e5dc88aab9901fb6ba401404
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-07-01 07:32:23 +0200

    print selected strategy

--- src/AutomateVideoPublishing/Program.cs
+++ src/AutomateVideoPublishing/Program.cs
@@ -40,6 +40,7 @@ class Program
         var contextResult = WorkflowContext.Create(options.QuickTimeMasterDirectory,
                                                    options.PublishedMpeg4Directory,
                                                    options.PublishedMediaLocalDirectory)
+            .Tap(context => Logger.Info($"Start execution of {strategyResult.Value.SelectedStrategy}."))
             .Tap(context => Logger.Info($"Executing workflow with quick time masterfile directory: {context.QuickTimeMasterDirectory.Directory}"))
             .Tap(context => Logger.Info($"Executing workflow with published MPEG-4 directory: {context.PublishedMpeg4Directory.Directory}"));
         if (contextResult.IsFailure)
