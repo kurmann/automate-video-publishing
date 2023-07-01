750e2078d81e4066260b59974beecbf1d067763a
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-07-01 15:48:48 +0200

    finally a parsing that works with mulitline #13

--- src/AutomateVideoPublishing/Entities/Metadata/Mpeg4MetadataCollection.cs
+++ src/AutomateVideoPublishing/Entities/Metadata/Mpeg4MetadataCollection.cs
@@ -8,80 +8,47 @@ public class Mpeg4MetadataCollection
     private Dictionary<string, string?> _metadata = new();
     private readonly Dictionary<string, string?> atoms;
 
-    /// <summary>
-    /// Liefert ein Dictionary, das die Atome und ihre entsprechenden Werte enthält.
-    /// </summary>
     public IReadOnlyDictionary<string, string?> Metadata => _metadata;
 
     private Mpeg4MetadataCollection(Dictionary<string, string?> atoms) => this.atoms = atoms;
 
     public static Result<Mpeg4MetadataCollection> Create(IEnumerable<string> lines)
     {
-        var enumerator = lines.GetEnumerator();
         var atoms = new Dictionary<string, string?>();
-        while (enumerator.MoveNext())
-        {
-            var line = enumerator.Current;
-            var (atom, value) = ParseLine(line, enumerator);
-            if (atom != null)
-            {
-                if (!atoms.ContainsKey(atom) || (atoms[atom] == null && value != null))
-                {
-                    atoms[atom] = value;
-                }
-            }
+        string lastAtom = string.Empty;
 
-            // If the line doesn't start with "Atom", it means we've gone too far so we need to step back
-            if (!line.StartsWith("Atom"))
+        foreach (var line in lines)
+        {
+            var parsedLine = GetParsedAtomicParsleyLine(line);
+
+            if (parsedLine.HasValue)
             {
-                // Step back
-                while (enumerator.Current != null && !enumerator.Current.StartsWith("Atom"))
+                lastAtom = parsedLine.Value.Key;
+                atoms[lastAtom] = parsedLine.Value.Value;
+            }
+            else
+            {
+                if (atoms.ContainsKey(lastAtom))
                 {
-                    enumerator.MoveNext();
+                    atoms[lastAtom] += Environment.NewLine + line;
                 }
             }
         }
-
-        var n = new KeyValuePair
 
         return Result.Success(new Mpeg4MetadataCollection(atoms));
     }
 
-
-
-
-    public static (string? Atom, string? Value) ParseLine(string line, IEnumerator<string> enumerator)
+    private static KeyValuePair<string, string?>? GetParsedAtomicParsleyLine(string line)
     {
         var match = Regex.Match(line, @"Atom ""(.*?)"" contains: (.*)");
-        string? atom = null;
-        var valueBuilder = new StringBuilder();
 
         if (match.Success)
         {
-            atom = match.Groups[1].Value;
-            valueBuilder.Append(match.Groups[2].Value);
+            var atom = match.Groups[1].Value;
+            var value = match.Groups[2].Value;
+            return new KeyValuePair<string, string?>(atom, value);
         }
 
-        // Check for multi-line content
-        while (enumerator.MoveNext())
-        {
-            line = enumerator.Current;
-
-            // If the line starts with "Atom", it's a new atom so stop collecting lines
-            if (line.StartsWith("Atom"))
-            {
-                break;
-            }
-
-            // Append the current line to the value
-            valueBuilder.AppendLine(line);
-        }
-
-        return (atom, valueBuilder.ToString());
+        return null;
     }
-
-
-
-
-
 }
