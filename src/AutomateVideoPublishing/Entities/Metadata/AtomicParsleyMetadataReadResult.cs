3e747eee5538565ebe39a63e02b437205c3899ce
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-07-01 10:08:14 +0200

    AtomicParsleyMetadataReadResult #13

---
+++ src/AutomateVideoPublishing/Entities/Metadata/AtomicParsleyMetadataReadResult.cs
@@ -0,0 +1,22 @@
+namespace AutomateVideoPublishing.Entities.Metadata;
+
+public class AtomicParsleyMetadataReadResult
+{
+    private List<string> lines = new List<string>();
+
+    public FileInfo FileInfo { get; }
+    public IReadOnlyList<string> Lines => lines.AsReadOnly();
+    private AtomicParsleyMetadataReadResult(FileInfo fileInfo) => FileInfo = fileInfo;
+
+    public static Result<AtomicParsleyMetadataReadResult> Create(FileInfo fileInfo, List<string> lines)
+    {
+        if (!fileInfo.Exists)
+        {
+            return Result.Failure<AtomicParsleyMetadataReadResult>($"File does not exist: {fileInfo.FullName}");
+        }
+
+        return Result.Success(new AtomicParsleyMetadataReadResult(fileInfo));
+    }
+
+    public void AddLine(string line) => lines.Add(line);
+}
