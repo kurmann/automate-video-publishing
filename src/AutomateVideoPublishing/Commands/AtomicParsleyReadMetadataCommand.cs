92c7437616b55928c7beadc62f7c65a598f0fb82
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-07-02 07:00:28 +0200

    simplified read metadata command #13

--- src/AutomateVideoPublishing/Commands/AtomicParsleyReadMetadataCommand.cs
+++ src/AutomateVideoPublishing/Commands/AtomicParsleyReadMetadataCommand.cs
@@ -1,57 +1,28 @@
-using System.Diagnostics;
+using AutomateVideoPublishing.Managers;
+using AutomateVideoPublishing.Entities.AtomicParsley;
+using System;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 
-namespace AutomateVideoPublishing.Commands;
-
-/// <summary>
-/// Führt den AtomicParsley-Befehl aus und übermittelt die Zeilenausgabe an ein Observable.
-/// </summary>
-public class AtomicParsleyReadMetadataCommand
+namespace AutomateVideoPublishing.Commands
 {
-    private readonly Subject<string> _subject = new Subject<string>();
-
-    /// <summary>
-    /// Observable, das die Zeilenausgabe von AtomicParsley verbreitet.
-    /// </summary>
-    public IObservable<string> Lines => _subject.AsObservable();
-
-    /// <summary>
-    /// Führt den AtomicParsley-Befehl für eine gegebene Datei aus.
-    /// </summary>
-    /// <param name="filePath">Der Pfad zur Datei, für die AtomicParsley ausgeführt werden soll.</param>
-    public void Run(string filePath)
+    public class AtomicParsleyReadMetadataCommand
     {
-        var psi = new ProcessStartInfo("AtomicParsley", $"\"{filePath}\" -t")
-        {
-            RedirectStandardOutput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true
-        };
+        private readonly Subject<string> _subject = new Subject<string>();
+        private readonly ProcessManager _processManager;
+        private readonly string _atomicParsleyPath = "AtomicParsley";
 
-        var process = new Process
-        {
-            StartInfo = psi,
-            EnableRaisingEvents = true
-        };
+        public AtomicParsleyReadMetadataCommand() => _processManager = new ProcessManager();
 
-        process.OutputDataReceived += (sender, e) =>
-        {
-            if (e.Data != null)
-            {
-                _subject.OnNext(e.Data);
-            }
-            else
-            {
-                // Wenn es keine Daten mehr gibt, schließen wir das Observable.
-                _subject.OnCompleted();
-            }
-        };
+        public IObservable<string> Lines => _subject.AsObservable();
 
-        process.Start();
-        process.BeginOutputReadLine();
-        process.WaitForExit();  // Warte, bis der Prozess beendet ist.
+        public void Run(string filePath)
+        {
+            var arguments = new AtomicParsleyArguments()
+                                .AddFilePath(filePath)
+                                .AddOption("-t");
+
+            _processManager.StartNewProcess(_atomicParsleyPath, arguments.Build, _subject);
+        }
     }
-
-
 }
