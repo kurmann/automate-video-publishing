f9ef322ad8923caeeeba8b7f91fa7861d6b11953
Patrick Kurmann <kurmannwillisau@gmail.com>
2023-07-03 23:06:05 +0200

    defined dedicated atomic parsley output type #13

---
+++ src/AutomateVideoPublishing/Entities/AtomicParsley/AtomicParsleyMetadataReadOutputLine.cs
@@ -0,0 +1,23 @@
+namespace AutomateVideoPublishing.Entities.AtomicParsley;
+
+public class AtomicParsleyMetadataReadOutputLine : ValueObject
+{
+    public string Value { get; }
+
+    private AtomicParsleyMetadataReadOutputLine(string value) => Value = value;
+
+    public static AtomicParsleyMetadataReadOutputLine Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            new AtomicParsleyMetadataReadOutputLine(string.Empty);
+        }
+
+        return new AtomicParsleyMetadataReadOutputLine(value);
+    }
+
+    protected override IEnumerable<string> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
